d502398fefd4c5afb2220a67fd98b9184fb017d0
csmall9 <csmall9@verizon.net>
2014-08-10 11:15:23 -0400

    Added file containing only Feb 1 and 2 data; added plot2 files

---
+++ plot2.R
@@ -0,0 +1,29 @@
+## Project for Exploratory Data Analysis
+
+##  This file genenates plot2 for project 1
+  
+  
+##  Step 1 - Load the data
+  
+  power <- read.csv2 ("household_power_consumption.txt")
+  head(power)
+  
+  powerfeb <- read.csv ("febpower2.csv")
+  
+  ## Note:  The filtering of data to Feb 1 and 2, 2007 in in plot1.R
+  
+  ## need to change text date & time 
+  
+  newdatetime <- strptime(paste(powerfeb$Date,powerfeb$Time), format= "%d/%m/%Y %H:%M:%S")
+
+  ## add this column to the data
+  newpowerfeb <- cbind(powerfeb,newdatetime)
+  
+  ##  Plot the data and save the file
+  
+  with(newpowerfeb, plot(newdatetime, Global_active_power,type = "l",ylab="Global Active Power (kilowatts)",xlab= " " ))
+  
+  dev.copy(png, file = "plot2.png")  ## Copy  plot 2 to a PNG file
+  dev.off()   ## close the PNG device.
+  
+  
